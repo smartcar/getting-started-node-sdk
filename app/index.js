6a6cb8b7286a433ab0b004b49cca192932377a0b
Karthik Bhaskara <karthikbhaskara96@gmail.com>
2018-11-27 16:18:57 -0800

    add scope parameter

--- app/index.js
+++ app/index.js
@@ -12,6 +12,7 @@ const client = new smartcar.AuthClient({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   redirectUri: process.env.REDIRECT_URI,
+  scope: ['read_vehicle_info'],
   testMode: true,
 });
 
